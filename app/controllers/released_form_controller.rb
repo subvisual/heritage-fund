42146955cd9500d1a185c6bafe9812eedfc5f179
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-10-28 17:22:57 +0000

    released forms

--- app/controllers/released_form_controller.rb
+++ app/controllers/released_form_controller.rb
@@ -2,8 +2,8 @@ class ReleasedFormController < ApplicationController
     protect_from_forgery with: :null_session
     def receive
        @project = Project.find(params[:ApplicationId])
-        if params[:Status] == 'Awarded' 
-            @project.released_forms.create(payload: params, form_type: 'permission-to-start')
+        if params[:Status] == 'Awarded' && !params[:ApprovedbyFinance]
+            @project.released_forms.create(payload: request.raw_post, form_type: 'permission-to-start')
         end
     end
 end
--- test/controllers/released_form_controller_test.rb
+++ test/controllers/released_form_controller_test.rb
@@ -5,13 +5,18 @@ class ReleasedFormControllerTest < ActionDispatch::IntegrationTest
 
   test "stub project exists" do
     assert_equal 1, Project.count
-  end
+  end                                
 
   test "webhook releases permission to start" do
     application_id = '2c660111-ab15-4221-98e0-cf0e02748a9b'
     post '/consumer', params: {"Status"=>"Awarded", "ProjectCosts"=>["PC-000095"], "forms"=>[], "Email"=>"lizzie90@example.com", "ContactName"=>"Nelda", "ApprovedPurpose"=>{}, "ApplicationStages"=>"Finance Approval", "ApplicationId"=>application_id, "AccountName"=>"New web test org", "AccountExternalID"=>"5188ff34-9011-4458-a9ea-19fe1ac9f3c6", "released_form"=>{}}
     assert Project.find(application_id).released_forms.first.form_type = 'permission-to-start'
-    assert_equal 204, status
+  end
+
+  test "webhook with finance does not release permission to start" do
+    application_id = '2c660111-ab15-4221-98e0-cf0e02748a9b'
+    post '/consumer', params: {"Status"=>"Awarded", "ProjectCosts"=>["PC-000095"], "forms"=>[], "Email"=>"lizzie90@example.com", "ContactName"=>"Nelda", "ApprovedPurpose"=>{}, "ApplicationStages"=>"Finance Approval", "ApplicationId"=>application_id, "AccountName"=>"New web test org", "AccountExternalID"=>"5188ff34-9011-4458-a9ea-19fe1ac9f3c6", "released_form"=>{}, "ApprovedbyFinance"=>true}
+    assert Project.find(application_id).released_forms.empty?
   end
 
 
