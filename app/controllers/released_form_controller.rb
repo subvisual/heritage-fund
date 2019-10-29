c02c24705442a4d3a8b2ccf91b275aee3e51dd02
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-10-29 11:12:11 +0000

    upload payload and test

--- app/controllers/released_form_controller.rb
+++ app/controllers/released_form_controller.rb
@@ -2,8 +2,11 @@ class ReleasedFormController < ApplicationController
     protect_from_forgery with: :null_session
     def receive
        @project = Project.find(params[:ApplicationId])
-        if params[:Status] == 'Awarded' && !params[:ApprovedbyFinance]
+       if params[:GrantDecision] == 'Awarded' && !params[:ApprovedbyFinance]
             @project.released_forms.create(payload: request.raw_post, form_type: 'permission-to-start')
+        end
+       if params[:ApprovedbyFinance]
+            @project.released_forms.create(payload: request.raw_post, form_type: 'completion-report')
         end
     end
 end
--- test/controllers/released_form_controller_test.rb
+++ test/controllers/released_form_controller_test.rb
@@ -9,14 +9,14 @@ class ReleasedFormControllerTest < ActionDispatch::IntegrationTest
 
   test "webhook releases permission to start" do
     application_id = '2c660111-ab15-4221-98e0-cf0e02748a9b'
-    post '/consumer', params: {"Status"=>"Awarded", "ProjectCosts"=>["PC-000095"], "forms"=>[], "Email"=>"lizzie90@example.com", "ContactName"=>"Nelda", "ApprovedPurpose"=>{}, "ApplicationStages"=>"Finance Approval", "ApplicationId"=>application_id, "AccountName"=>"New web test org", "AccountExternalID"=>"5188ff34-9011-4458-a9ea-19fe1ac9f3c6", "released_form"=>{}}
+    post '/consumer', params: {"GrantDecision"=>"Awarded", "ProjectCosts"=>["PC-000095"], "forms"=>[], "Email"=>"lizzie90@example.com", "ContactName"=>"Nelda", "ApprovedPurpose"=>{}, "ApplicationStages"=>"Finance Approval", "ApplicationId"=>application_id, "AccountName"=>"New web test org", "AccountExternalID"=>"5188ff34-9011-4458-a9ea-19fe1ac9f3c6", "released_form"=>{}}
     assert Project.find(application_id).released_forms.first.form_type = 'permission-to-start'
   end
 
   test "webhook with finance does not release permission to start" do
     application_id = '2c660111-ab15-4221-98e0-cf0e02748a9b'
     post '/consumer', params: {"Status"=>"Awarded", "ProjectCosts"=>["PC-000095"], "forms"=>[], "Email"=>"lizzie90@example.com", "ContactName"=>"Nelda", "ApprovedPurpose"=>{}, "ApplicationStages"=>"Finance Approval", "ApplicationId"=>application_id, "AccountName"=>"New web test org", "AccountExternalID"=>"5188ff34-9011-4458-a9ea-19fe1ac9f3c6", "released_form"=>{}, "ApprovedbyFinance"=>true}
-    assert Project.find(application_id).released_forms.empty?
+    assert Project.find(application_id).released_forms.first.form_type = 'completion-report'
   end
 
 
