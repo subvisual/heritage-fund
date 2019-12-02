0ba7192f7b7d183a3a043ce1088c299fc127ea58
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-12-02 16:20:08 +0000

    fix migration syntax

--- db/migrate/20191014131515_make_organisation_id_optional.rb
+++ db/migrate/20191014131515_make_organisation_id_optional.rb
@@ -3,6 +3,6 @@ class MakeOrganisationIdOptional < ActiveRecord::Migration[6.0]
     execute <<-SQL
     CREATE TYPE form_type AS ENUM ('local-authority', 'other-public-sector-organisation', 'registered-charity', 'faith-based-or-church-organisation', 'private-owner-of-heritage' );
     SQL
-    change_column :users, :organisations_id, :uuid\, :null => true
+    change_column :users, :organisations_id, :uuid, :null => true
   end
 end
