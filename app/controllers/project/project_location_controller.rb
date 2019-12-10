a52d70cf30bb55a9b7a5a89ac2ad2ee7cd9f8204
PushBackNeil <neil.hathaway@pushbacksoftware.co.uk>
2019-12-10 18:11:55 +0000

    conditional redirects based on yes/no answer to same location question

--- app/controllers/project/project_location_controller.rb
+++ app/controllers/project/project_location_controller.rb
@@ -4,4 +4,16 @@ class Project::ProjectLocationController < ApplicationController
 
   def project_location_no
   end
+
+  def save_project_location
+    same_location = params['same-location'].to_s == 'yes' ? true : false unless params['same-location'].nil?
+
+    # todo really save same_location data
+
+    if (same_location == true)
+      redirect_to '/project/description'
+    else
+      redirect_to '/project/other-location'
+    end
+  end
 end
