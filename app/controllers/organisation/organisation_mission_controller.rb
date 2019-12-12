00c120ca14701088d9eb72e29c97a4c140819305
Stuart McColl <contact@stuartmccoll.co.uk>
2019-12-12 16:01:31 +0000

    Updated organisation mission controller with some validation logic

--- app/controllers/organisation/organisation_mission_controller.rb
+++ app/controllers/organisation/organisation_mission_controller.rb
@@ -6,33 +6,36 @@ class Organisation::OrganisationMissionController < ApplicationController
   before_action :authenticate_user!
 
   def show
-
     @organisation = Organisation.find(current_user.organisation.id)
-
     render_wizard
-
   end
 
   def update
 
-    @organisation = Organisation.find(current_user.organisation.id)
+    # This information about an organisation doesn't have to be provided,
+    # therefore we only want to update the model if the user has selected
+    # a form option. We can tell whether this has been done by checking
+    # for the presence of an organisation hash.
+    if !params[:organisation].nil?
+      @organisation = Organisation.find(current_user.organisation.id)
 
-    logger.debug 'Updating organisation ' + @organisation.id +
-                     ', setting mission to ' + params[:organisation][:mission]
+      logger.debug 'Updating organisation ' + @organisation.id +
+                       ', setting mission to ' + params[:organisation][:mission]
 
-    @organisation = Organisation.update(current_user.organisation.id,
-                                        :mission => params[:organisation][:mission])
+      @organisation = Organisation.update(current_user.organisation.id,
+                                          :mission => params[:organisation][:mission])
 
-    logger.debug 'Finished updating organisation ' + @organisation.id
+      logger.debug 'Finished updating organisation ' + @organisation.id
+    else
+      logger.debug 'No mission added for organisation ' + current_user.organisation.id
+    end
 
     redirect_to_finish_wizard
 
   end
 
   def finish_wizard_path
-
     organisation_organisation_signatories_get_path(current_user.organisation.id, id: 'signatories')
-
   end
 
 end
