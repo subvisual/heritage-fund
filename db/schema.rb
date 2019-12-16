751303d476ee97c5f6fcaaa8b6acdaf2ab0f941b
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-12-16 12:07:02 +0000

    add unconfirmed_email column

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_12_16_113731) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
