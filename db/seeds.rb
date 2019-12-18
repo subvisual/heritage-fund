2fa8a551d1a22f43981b83d7150447b30640c14b
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-12-18 15:35:41 +0000

    add seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'test@example.com', encrypted_password: User.new.send(:password_digest, '123456')).confirm
