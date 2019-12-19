3611bb12cb3b13cfd3ec3c1dee24d3264ba0c0c1
Stuart McColl <contact@stuartmccoll.co.uk>
2019-12-19 08:13:34 +0000

    Updated organisation type validation

--- app/controllers/organisation/organisation_type_controller.rb
+++ app/controllers/organisation/organisation_type_controller.rb
@@ -2,49 +2,47 @@ class Organisation::OrganisationTypeController < ApplicationController
   include OrganisationHelper
   before_action :authenticate_user!, :set_organisation
 
-  @@error_message = "Select the type of organisation that will be running your project"
-
   def show
     render :type
   end
 
   def update
+
+    logger.debug "Attempting to update organisation ID: #{@organisation.id} "
+
     @organisation.validate_org_type = true
 
-    if !params[:organisation].present?
+    @organisation.update(organisation_type_params)
+
+    if @organisation.valid?
+
+      redirect_to :organisation_organisation_numbers_get
+
+    else
 
       logger.debug "Organisation type not found when attempting to update organisation ID: " +
                        "#{@organisation.id}"
 
-      flash[:alert] = @@error_message
+      flash[:alert] = @organisation.errors[:org_type][0]
 
-    else
-
-      logger.debug "Attempting to updating organisation ID: #{@organisation.id} " +
-                       "- setting org_type to #{params[:organisation][:org_type]}"
-
-      if !@organisation.update(organisation_type_params)
-
-        logger.debug "Error occurred when updating organisation ID: #{@organisation.id}"
-
-        flash[:alert] = @@error_message
-
-      else
-
-        logger.debug "Finished updating organisation ID: #{@organisation.id}"
-
-      end
+      redirect_to :organisation_organisation_type_get
 
     end
 
-    redirect_to flash[:alert].present? ? :organisation_organisation_type_get : :organisation_organisation_numbers_get
+    logger.debug "Finished updating organisation ID: #{@organisation.id}"
 
   end
 
   private
 
   def organisation_type_params
+
+    if !params[:organisation].present?
+      params.merge!({organisation: {org_type: ""}})
+    end
+
     params.require(:organisation).permit(:org_type)
+
   end
 
 end
