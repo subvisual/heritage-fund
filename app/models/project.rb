dd319260a6849d454bca7436a35cc5e7bc376bc2
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-12-30 16:29:05 +0000

    create association between project and organisation through user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
     belongs_to :user
+    has_one :organisation, through: :user
     has_many :released_forms
     has_many_attached :evidence_of_support_files
 
@@ -14,30 +15,30 @@ class Project < ApplicationRecord
                 json.set!('username', self.user.email)
             end
             json.application do
-                json.set!('organisationName', self.user.organisation.name)
+                json.set!('organisationName', self.organisation.name)
                 json.set!('organisationMission',
-                          self.user.organisation.mission.map!(&:dasherize).map { |m| m == 'lgbt-plus-led' ? 'lgbt+-led' : m })
-                json.set!('organisationType', self.user.organisation.org_type.dasherize)
-                json.set!('companyNumber', self.user.organisation.company_number&.to_s)
-                json.set!('charityNumber', self.user.organisation.charity_number&.to_s)
+                          self.organisation.mission.map!(&:dasherize).map { |m| m == 'lgbt-plus-led' ? 'lgbt+-led' : m })
+                json.set!('organisationType', self.organisation.org_type.dasherize)
+                json.set!('companyNumber', self.organisation.company_number&.to_s)
+                json.set!('charityNumber', self.organisation.charity_number&.to_s)
                 json.organisationAddress do
-                    json.line1 self.user.organisation.line1
-                    json.line2 self.user.organisation.line2
-                    json.line3 self.user.organisation.line3
-                    json.county self.user.organisation.county
-                    json.townCity self.user.organisation.townCity
-                    json.postcode self.user.organisation.postcode
+                    json.line1 self.organisation.line1
+                    json.line2 self.organisation.line2
+                    json.line3 self.organisation.line3
+                    json.county self.organisation.county
+                    json.townCity self.organisation.townCity
+                    json.postcode self.organisation.postcode
                 end
                 set_legal_signatory_fields = ->(ls) {
                     json.set!("name", ls.name)
                     json.set!("email", ls.email_address)
                     json.set!("phone", ls.phone_number)
                 }
-                @ls_one = self.user.organisation.legal_signatories.first
+                @ls_one = self.organisation.legal_signatories.first
                 json.authorisedSignatoryOneDetails do
                     set_legal_signatory_fields.call(@ls_one)
                 end
-                @ls_two = self.user.organisation.legal_signatories.second
+                @ls_two = self.organisation.legal_signatories.second
                 if @ls_two.present?
                     json.authorisedSignatoryTwoDetails do
                         set_legal_signatory_fields.call(@ls_two)
