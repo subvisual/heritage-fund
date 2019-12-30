94eb93a3e5f112e3ed132138c6b9e5df86e7883a
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-12-30 14:52:11 +0000

    Add Salesforce serialisation of organisation fields (#51)
    * add Salesforce serialiser to user model
    
    * build up json payload
    
    * add legal signatories
    
    * add charity and company number, ignore nil fields
    
    * add towncityfield

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,48 @@ class Project < ApplicationRecord
     belongs_to :user
     has_many :released_forms
     has_many_attached :evidence_of_support_files
+
+    def to_salesforce_json
+        Jbuilder.encode do |json|
+            json.ignore_nil!
+            json.meta do
+                json.set!('applicationId', self.id)
+                json.set!('form', "3-10k-grant")
+                json.set!('schemaVersion', "v1.x")
+                json.set!('startedAt', self.created_at)
+                json.set!('username', self.user.email)
+            end
+            json.application do
+                json.set!('organisationName', self.user.organisation.name)
+                json.set!('organisationMission',
+                          self.user.organisation.mission.map!(&:dasherize).map { |m| m == 'lgbt-plus-led' ? 'lgbt+-led' : m })
+                json.set!('organisationType', self.user.organisation.org_type.dasherize)
+                json.set!('companyNumber', self.user.organisation.company_number&.to_s)
+                json.set!('charityNumber', self.user.organisation.charity_number&.to_s)
+                json.organisationAddress do
+                    json.line1 self.user.organisation.line1
+                    json.line2 self.user.organisation.line2
+                    json.line3 self.user.organisation.line3
+                    json.county self.user.organisation.county
+                    json.townCity self.user.organisation.townCity
+                    json.postcode self.user.organisation.postcode
+                end
+                set_legal_signatory_fields = ->(ls) {
+                    json.set!("name", ls.name)
+                    json.set!("email", ls.email_address)
+                    json.set!("phone", ls.phone_number)
+                }
+                @ls_one = self.user.organisation.legal_signatories.first
+                json.authorisedSignatoryOneDetails do
+                    set_legal_signatory_fields.call(@ls_one)
+                end
+                @ls_two = self.user.organisation.legal_signatories.second
+                if @ls_two.present?
+                    json.authorisedSignatoryTwoDetails do
+                        set_legal_signatory_fields.call(@ls_two)
+                    end
+                end
+            end
+        end
+    end
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "serialises Salesforce JSON" do
+     @project = projects(:one)
+     @project_salesforce_json_obj = JSON.parse(@project.to_salesforce_json)
+     assert_equal  '2c660111-ab15-4221-98e0-cf0e02748a9b', @project_salesforce_json_obj['meta']['applicationId']
+     assert_equal 'test@example.com', @project_salesforce_json_obj['meta']['username']
+     assert_equal 'test organisation', @project_salesforce_json_obj['application']['organisationName']
+     assert_equal 'registered-charity', @project_salesforce_json_obj['application']['organisationType']
+     assert_equal 'line 1', @project_salesforce_json_obj['application']['organisationAddress']['line1']
+     assert_equal 'town', @project_salesforce_json_obj['application']['organisationAddress']['townCity']
+     assert_equal 'John Doe', @project_salesforce_json_obj['application']['authorisedSignatoryOneDetails']['name']
+     assert_equal 'Jane Doe', @project_salesforce_json_obj['application']['authorisedSignatoryTwoDetails']['name']
+     assert_equal '123', @project_salesforce_json_obj['application']['charityNumber']
+     assert_equal %w(black-or-minority-ethnic-led disability-led lgbt+-led female-led young-people-led), @project_salesforce_json_obj['application']['organisationMission']
+   end
 end
