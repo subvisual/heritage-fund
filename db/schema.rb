e3b7b9eb2979e01090dfb8b6c64ad8cbe5837d36
Stuart McColl <contact@stuartmccoll.co.uk>
2019-12-31 10:49:18 +0000

    Added migration file to add dates to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_133056) do
+ActiveRecord::Schema.define(version: 2019_12_31_104443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2019_12_19_133056) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "evidence_description", default: [], array: true
+    t.date "start_date"
+    t.date "end_date"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
