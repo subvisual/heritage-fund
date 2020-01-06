d82be3ab6870b74b72367bf2ead1efba924d337a
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2020-01-06 10:18:25 +0000

    add initial model and migrations for volunteers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_080153) do
+ActiveRecord::Schema.define(version: 2020_01_06_101655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -135,9 +135,19 @@ ActiveRecord::Schema.define(version: 2020_01_06_080153) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "volunteers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "description"
+    t.integer "hours"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "project_id", null: false
+    t.index ["project_id"], name: "index_volunteers_on_project_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cash_contributions", "projects"
   add_foreign_key "project_costs", "projects"
   add_foreign_key "projects", "users"
   add_foreign_key "users", "organisations"
+  add_foreign_key "volunteers", "projects"
 end
