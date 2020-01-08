258a8cd29d5b93da0298743d1b54c4e74c43ac40
PushBackNeil <neil.hathaway@pushbacksoftware.co.uk>
2020-01-08 15:48:01 +0000

    revert schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_154043) do
+ActiveRecord::Schema.define(version: 2020_01_07_095122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.uuid "record_id", null: false
+    t.uuid "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -107,7 +107,6 @@ ActiveRecord::Schema.define(version: 2020_01_07_154043) do
     t.text "involvement_description"
     t.integer "permission_type"
     t.text "permission_description"
-    t.boolean "capital_work"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
