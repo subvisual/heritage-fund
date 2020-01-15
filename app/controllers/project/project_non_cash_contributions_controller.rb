653f0da3a34fc0d4761ba3cba90dadfc26394714
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-15 17:38:12 +0000

    Added initial form and controller functionality for non-cash contributions

--- app/controllers/project/project_non_cash_contributions_controller.rb
+++ app/controllers/project/project_non_cash_contributions_controller.rb
@@ -1,13 +1,23 @@
 class Project::ProjectNonCashContributionsController < ApplicationController
+  include ProjectContext
+
+  def show
+    @non_cash_contributions = @project.non_cash_contributions.build
+  end
 
   def update
+
+    # TODO: Validate params
+    @project.update(project_params)
+
+    redirect_to three_to_ten_k_project_project_non_cash_contributions_path
 
   end
 
   private
 
   def project_params
-
+    params.require(:project).permit(non_cash_contributions_attributes: [:description, :amount])
   end
 
 end
