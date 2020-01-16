f8420451acf6c83556cbda81f40e8130c1f70a0b
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-16 15:01:05 +0000

    Validation added for non-cash contributions page

--- app/controllers/project/project_non_cash_contributions_controller.rb
+++ app/controllers/project/project_non_cash_contributions_controller.rb
@@ -1,16 +1,35 @@
 class Project::ProjectNonCashContributionsController < ApplicationController
   include ProjectContext
 
-  def show
-    @non_cash_contributions = @project.non_cash_contributions.build
-  end
-
   def update
 
-    # TODO: Validate params
+    # Empty flash values to ensure that we don't redisplay them unnecessarily
+    flash[:description_value] = ""
+    flash[:amount] = ""
+
+    logger.debug "Adding non-cash contribution for project ID: #{@project.id}"
+
+    @project.validate_non_cash_contributions = true
+
     @project.update(project_params)
 
-    redirect_to three_to_ten_k_project_project_non_cash_contributions_path
+    if @project.valid?
+
+      logger.debug "Successfully added non-cash contribution for project ID: #{@project.id}"
+
+      redirect_to three_to_ten_k_project_non_cash_contributions_get_path
+
+    else
+
+      logger.debug "Validation failed when adding non-cash contribution for project ID: #{@project.id}"
+
+      # Store flash values to display them again when re-rendering the page
+      flash[:description] = params['project']['non_cash_contributions_attributes']['0']['description']
+      flash[:amount] = params['project']['non_cash_contributions_attributes']['0']['amount']
+
+      render :show
+
+    end
 
   end
 
