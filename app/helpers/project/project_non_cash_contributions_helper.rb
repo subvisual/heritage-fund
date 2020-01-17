64355e366e6629434efbe7705333ba27ed36fae3
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-17 14:22:12 +0000

    Added dynamic calculation of non-cash contributions to view

--- app/helpers/project/project_non_cash_contributions_helper.rb
+++ app/helpers/project/project_non_cash_contributions_helper.rb
@@ -1,2 +1,5 @@
 module Project::ProjectNonCashContributionsHelper
+  def calculate_total(non_cash_contributions)
+    non_cash_contributions.select(:amount).map(&:amount).compact.reduce(:+)
+  end
 end
