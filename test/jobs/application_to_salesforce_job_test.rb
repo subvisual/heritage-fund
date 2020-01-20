2e35dfe86b881c9569eb0f976a3182854b1679d1
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2020-01-20 18:06:38 +0000

    add test for happy path updating database.

--- test/jobs/application_to_salesforce_job_test.rb
+++ test/jobs/application_to_salesforce_job_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
-include ActiveJob::TestHelper
 class ApplicationToSalesforceJobTest < ActiveSupport::TestCase
 
   def setup
     stub_request(:post, "https://test.salesforce.com/services/oauth2/token").to_return(status: 200, body:
         '{"instance_url":"https://example.salesforce.com/"}')
-  WebMock.disable_net_connect!(allow_localhost: true)
+  WebMock.disable_net_connect!
   end
 
   test 'failure response throws exception' do
@@ -15,5 +14,17 @@ class ApplicationToSalesforceJobTest < ActiveSupport::TestCase
     assert_raises ApplicationToSalesforceJob::SalesforceApexError do
       ApplicationToSalesforceJob.perform_now(project)
     end
+  end
+
+  test 'success case updates database' do
+    stub_request(:post, "https://example.salesforce.com/services/apexrest/PortalData").
+        to_return(status: 200, body: {"statusCode"=>"200", "status"=>"Success", "projectRefNumber"=>"NS-19-01498", "ProjectCostRefID"=>nil, "Costs"=>nil, "Costheading"=>nil, "caseNumber"=>"00001498", "caseId"=>"5002500000BFRUiAAP", "accountId"=>"0012500001I6ImfAAF"}.to_json)
+    project = projects(:one)
+    ApplicationToSalesforceJob.perform_now(project)
+    assert_equal("NS-19-01498", project.project_reference_number)
+    assert_equal("5002500000BFRUiAAP", project.salesforce_case_id)
+    assert_equal("0012500001I6ImfAAF", project.organisation.salesforce_account_id)
+    assert_equal("00001498", project.salesforce_case_number)
+
   end
 end
