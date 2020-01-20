7da50a56baff0bd6be82c08166f130110aaeb9d5
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2020-01-20 12:53:33 +0000

    write fields to DB from salesforce response

--- app/jobs/application_to_salesforce_job.rb
+++ app/jobs/application_to_salesforce_job.rb
@@ -1,11 +1,13 @@
 require 'restforce'
 
 class ApplicationToSalesforceJob < ApplicationJob
+  class SalesforceApexError < StandardError; end
   queue_as :default
 
   # @param [Project] project
   def perform(project)
-    puts(project.id)
+    logger.info("Submitting Project ID: #{project.id} to Salesforce")
+    project.update(submitted_on: DateTime.now)
     client = Restforce.new(
         username: Rails.configuration.x.salesforce.username,
         password: Rails.configuration.x.salesforce.password,
@@ -16,8 +18,20 @@ class ApplicationToSalesforceJob < ApplicationJob
         api_version: '48.0'
     )
     @json = project.to_salesforce_json
-    puts(@json)
-    @response = client.post('/services/apexrest/PortalData', project.to_salesforce_json,  {'Content-Type'=>'application/json'})
-    puts(@response&.body)
+    @response = client.post('/services/apexrest/PortalData', @json,  {'Content-Type'=>'application/json'})
+    @response_body_obj = JSON.parse(@response&.body)
+    is_successful = @response_body_obj&.dig('status') == 'Success'
+    if(is_successful)
+      project.update(
+          salesforce_case_number: @response_body_obj.dig('caseNumber'),
+          salesforce_case_id: @response_body_obj.dig('caseId'),
+          project_reference_number: @response_body_obj.dig('projectRefNumber')
+      )
+      project.organisation.update(
+          salesforce_account_id: @response_body_obj.dig('accountId')
+      )
+    else
+      raise SalesforceApexError("Error response from Salesforce #{@response_body_obj}")
+    end
   end
 end
