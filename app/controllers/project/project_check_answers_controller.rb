b615684978161bb37c196c865aee375bac28de31
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-20 08:39:40 +0000

    Minor tidying of check your answers page

--- app/controllers/project/project_check_answers_controller.rb
+++ app/controllers/project/project_check_answers_controller.rb
@@ -1,9 +1,3 @@
 class Project::ProjectCheckAnswersController < ApplicationController
-
   include ProjectContext
-
-  def save_continue
-    redirect_to three_to_ten_k_project_confirm_declaration_get_path
-  end
-
 end
