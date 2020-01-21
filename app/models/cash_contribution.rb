f0950ecc9d4f990991941d7a6de8f566d467a322
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-21 17:11:51 +0000

    Finished adding validation to cash contributions page

--- app/models/cash_contribution.rb
+++ app/models/cash_contribution.rb
@@ -7,8 +7,38 @@ class CashContribution < ApplicationRecord
   validates :amount, numericality: { only_integer: true }
   validates_inclusion_of :secured, in: ["yes_with_evidence", "no", "x_not_sure", "yes_no_evidence_yet"]
 
-  # TODO: Validate inclusion of evidence files if
-  #       :secured == "yes_with_evidence"
+  validate do
+
+    validate_file_attached(
+        :cash_contribution_evidence_files,
+        "Add supporting evidence for a secured cash contribution with evidence"
+    ) if self[:secured] == "yes_with_evidence"
+
+    validate_length(
+        :description,
+        50,
+        "Description of your cash contribution must be 50 words or fewer"
+    )
+
+  end
+
+  def validate_file_attached(field, error_msg)
+    unless self.public_send(field).attached?
+      errors.add(field, error_msg)
+    end
+  end
+
+  def validate_length(field, max_length, error_msg)
+
+    word_count = self.public_send(field)&.split(' ')&.count
+
+    logger.debug "#{field} word count is #{word_count}"
+
+    if word_count && word_count > max_length
+      self.errors.add(field, error_msg)
+    end
+
+  end
 
   enum secured: {
       yes_with_evidence: 0,
