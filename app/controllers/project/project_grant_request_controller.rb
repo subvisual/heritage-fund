cb407ea9111c1804e8125750549e75601d8087fe
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-23 15:50:39 +0000

    Implemented fix to nil-value bug on grant request page
    This commit fixes a bug which could occur on the 'Your
    Grant Request' page if a user had navigated to this page
    without setting any project costs or cash contributions.
    
    Previously, the @final_grant_amount instance variable
    would have a value of nil, which would cause an error
    when the view attempted to use this variable.
    
    Setting .to_i on the values inside the @final_grant_amount
    calculation implicitly converts them to 0 if they are nil.
    
    As well, this commit adds an additional message to the
    'Your grant request' page in case a @final_grant_amount of 0
    is present.

--- app/controllers/project/project_grant_request_controller.rb
+++ app/controllers/project/project_grant_request_controller.rb
@@ -6,7 +6,7 @@ class Project::ProjectGrantRequestController < ApplicationController
     @total_project_cost = helpers.calculate_total(@project.project_costs)
     @total_cash_contributions = helpers.calculate_total(@project.cash_contributions)
 
-    @final_grant_amount = @total_project_cost - @total_cash_contributions
+    @final_grant_amount = @total_project_cost.to_i - @total_cash_contributions.to_i
 
   end
 
