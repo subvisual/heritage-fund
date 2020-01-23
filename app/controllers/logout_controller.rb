5b832e589e79e6a6e685877d79e34fdb32209e54
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2020-01-23 10:22:39 +0000

    remove spike, boilerplate code and tests

--- app/controllers/logout_controller.rb
+++
@@ -1,6 +0,0 @@
-class LogoutController < ApplicationController
-    include LogoutHelper
-    def logout
-        redirect_to logout_url.to_s
-    end
-end
--- test/controllers/postcode_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PostcodeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
