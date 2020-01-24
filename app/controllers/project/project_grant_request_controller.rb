3fe5d23cdb9d03709be000f9cd9dd09fcce8ca5f
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-24 15:34:39 +0000

    Update app/controllers/project/project_grant_request_controller.rb
    Co-Authored-By: Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>

--- app/controllers/project/project_grant_request_controller.rb
+++ app/controllers/project/project_grant_request_controller.rb
@@ -8,7 +8,7 @@ class Project::ProjectGrantRequestController < ApplicationController
     @final_grant_amount = @total_project_cost - @total_cash_contributions
     @maximum_request_value = 10000.to_i
     @costs_greater_than_contributions = @final_grant_amount.positive? 
-    @grant_request_less_than_funding_band = @final_grant_amount <= @maximum_request_value ? true : false
+    @grant_request_less_than_funding_band = @final_grant_amount <= @maximum_request_value 
     @grant_request_is_valid = @costs_greater_than_contributions && @grant_request_less_than_funding_band ? true : false
 
   end
