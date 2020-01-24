c733fe28af73244215d86594d61e8987be994ceb
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-24 11:48:20 +0000

    Added running of Salesforce job to declaration controller

--- app/controllers/project/project_declaration_controller.rb
+++ app/controllers/project/project_declaration_controller.rb
@@ -13,6 +13,8 @@ class Project::ProjectDeclarationController < ApplicationController
 
       logger.debug "Finished updating declaration confirmation for project ID: #{@project.id}"
 
+      ApplicationToSalesforceJob.perform_later(@project)
+
       redirect_to three_to_ten_k_project_application_submitted_get_path
 
     else
