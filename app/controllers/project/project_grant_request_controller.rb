4d44847eb0e54c73927a0161fa184af45c0d5bcf
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-24 15:24:14 +0000

    Refactored logic from view into controller

--- app/controllers/project/project_grant_request_controller.rb
+++ app/controllers/project/project_grant_request_controller.rb
@@ -5,8 +5,11 @@ class Project::ProjectGrantRequestController < ApplicationController
 
     @total_project_cost = helpers.calculate_total(@project.project_costs).to_i
     @total_cash_contributions = helpers.calculate_total(@project.cash_contributions).to_i
-
     @final_grant_amount = @total_project_cost - @total_cash_contributions
+    @maximum_request_value = 10000.to_i
+    @costs_greater_than_contributions = @final_grant_amount.positive? ? true : false
+    @grant_request_less_than_funding_band = @final_grant_amount <= @maximum_request_value ? true : false
+    @grant_request_is_valid = @costs_greater_than_contributions && @grant_request_less_than_funding_band ? true : false
 
   end
 
