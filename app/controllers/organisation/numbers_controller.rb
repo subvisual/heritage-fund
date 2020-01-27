0a23d3663dc4f92a15451cc7ca534e26364bea60
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-27 13:31:45 +0000

    Added missing question marks from key? method call

--- app/controllers/organisation/numbers_controller.rb
+++ app/controllers/organisation/numbers_controller.rb
@@ -10,8 +10,8 @@ class Organisation::NumbersController < ApplicationController
 
     # Neither field on this page is mandatory, so we only want to validate them
     # if they are passed through the form
-    @organisation.validate_company_number = true if params[:organisation].key("company_number")
-    @organisation.validate_charity_number = true if params[:organisation].key("charity_number")
+    @organisation.validate_company_number = true if params[:organisation].key?("company_number")
+    @organisation.validate_charity_number = true if params[:organisation].key?("charity_number")
 
     @organisation.update(organisation_params)
 
