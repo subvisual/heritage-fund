39cd2e221859a3e23fd40d3305d3509277ed6e28
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-27 11:32:13 +0000

    Tidied organisation type controller and view

--- app/controllers/organisation/type_controller.rb
+++ app/controllers/organisation/type_controller.rb
@@ -1,38 +1,43 @@
 class Organisation::TypeController < ApplicationController
-  include OrganisationContext
+  include OrganisationContext, ObjectErrorsLogger
 
+  # This method updates the org_type attribute of an organisation,
+  # redirecting to :organisation_numbers if successful and re-rendering
+  # :show method if unsuccessful
   def update
 
-    logger.debug "Attempting to update organisation ID: #{@organisation.id} "
+    logger.info "Updating org_type for organisation ID: #{@organisation.id}"
 
     @organisation.validate_org_type = true
 
-    @organisation.update(organisation_type_params)
+    @organisation.update(organisation_params)
 
     if @organisation.valid?
+
+      logger.info "Finished updating org_type for organisation ID: #{@organisation.id}"
 
       redirect_to :organisation_numbers
 
     else
 
-      logger.debug "Organisation type not found when attempting to update organisation ID: " +
-                       "#{@organisation.id}"
+      logger.info "Validation failed when attempting to update org_type for organisation ID: #{@organisation.id}"
+
+      log_errors(@organisation)
 
       render :show
 
     end
 
-    logger.debug "Finished updating organisation ID: #{@organisation.id}"
-
   end
 
   private
+  def organisation_params
 
-  def organisation_type_params
-
-    if !params[:organisation].present?
-      params.merge!({organisation: {org_type: ""}})
-    end
+    # When no radio button is selected on the page no org_type key/value is passed in the form,
+    # meaning that the organisation hash is no longer passed through either. In this case, we
+    # need to add it manually to avoid triggering a 'param is missing or value is empty'
+    # exception
+    params.merge!(organisation: {org_type: ""}) unless params[:organisation].present?
 
     params.require(:organisation).permit(:org_type)
 
