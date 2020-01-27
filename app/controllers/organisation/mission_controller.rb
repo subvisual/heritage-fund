151e5138f38473cd9d4b0df4e3ee0cfbc835815e
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-27 13:51:46 +0000

    General tidying of organisation mission section

--- app/controllers/organisation/mission_controller.rb
+++ app/controllers/organisation/mission_controller.rb
@@ -1,9 +1,12 @@
 class Organisation::MissionController < ApplicationController
-  include OrganisationContext
+  include OrganisationContext, ObjectErrorsLogger
 
+  # This method updates the mission attribute of an organisation,
+  # redirecting to :organisation_signatories if successful and re-rendering
+  # :show method if unsuccessful
   def update
 
-    logger.debug "Attempting to update organisation ID: #{@organisation.id} "
+    logger.info "Updating mission for organisation ID: #{@organisation.id}"
 
     @organisation.validate_mission = true
 
@@ -11,25 +14,29 @@ class Organisation::MissionController < ApplicationController
 
     if @organisation.valid?
 
+      logger.info "Finished updating mission for organisation ID: #{@organisation.id}"
+
       redirect_to :organisation_signatories
 
     else
 
-      logger.debug "Organisation type invalid when attempting to update organisation ID: " +
-                       "#{@organisation.id}"
+      logger.info "Validation failed when attempting to update mission for organisation ID: #{@organisation.id}"
+
+      log_errors(@organisation)
 
       render :show
 
     end
 
-    logger.debug "Finished updating organisation ID: #{@organisation.id}"
-
   end
 
   private
-
   def organisation_mission_params
 
+    # When no checkbox is selected on the page no mission key/value is passed in the form,
+    # meaning that the organisation hash is no longer passed through either. In this case, we
+    # need to add it manually to avoid triggering a 'param is missing or value is empty'
+    # exception
     if !params[:organisation].present?
       params.merge!({organisation: {mission: ""}})
     end
