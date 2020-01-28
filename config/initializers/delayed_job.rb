624033767343b1d02e6ef40c2e813f8070a21ae9
Stuart McColl <contact@stuartmccoll.co.uk>
2020-01-28 11:36:11 +0000

    Added staging environment

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+if Rails.env.production? || Rails.env.staging?
   Delayed::Worker.logger = Rails.logger
   Delayed::Worker.destroy_failed_jobs = false
   Delayed::Worker.max_attempts = 2
