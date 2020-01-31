41137178cbb4ac792e0d226674c1eb318a0a10cc
Paul Smith <mail@paulsmith.site>
2020-01-31 15:39:16 +0000

    adds browserlistrc settings and converts inline js from es6 to es5 (#249)
    * adds browserlistrc settings and converts inline js from es6 to es5
    
    * require polyfill js
    
    * updates babelified js to include IE10 preset
    
    Co-authored-by: Gideon Goldberg <gidsgoldberg@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/activestorage").start();
 require("channels");
 require("govuk");
 require("../sentry/index")
+require("core-js/stable");
+require("regenerator-runtime/runtime");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
