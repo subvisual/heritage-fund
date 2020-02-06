03348ba7f67c791c6b1c17071a9754b5040be441
Stuart McColl <contact@stuartmccoll.co.uk>
2020-02-06 09:38:04 +0000

    Implemented organisation mission fix and tidied controller
    Watching a Hotjar recording exposed a bug on the organisation
    mission page - when unchecking a checkbox and submitting the form,
    the value was not being unset on the database. This pull
    request fixes this bug, and tidies the code a little in order
    to pass RuboCop line length validation.

--- app/controllers/organisation/mission_controller.rb
+++ app/controllers/organisation/mission_controller.rb
@@ -10,17 +10,19 @@ class Organisation::MissionController < ApplicationController
 
     @organisation.validate_mission = true
 
-    @organisation.update(organisation_mission_params)
+    @organisation.update(organisation_params)
 
     if @organisation.valid?
 
-      logger.info "Finished updating mission for organisation ID: #{@organisation.id}"
+      logger.info "Finished updating mission for organisation ID: " \
+                  "#{@organisation.id}"
 
       redirect_to :organisation_signatories
 
     else
 
-      logger.info "Validation failed when attempting to update mission for organisation ID: #{@organisation.id}"
+      logger.info "Validation failed when attempting to update mission for " \
+                  "organisation ID: #{@organisation.id}"
 
       log_errors(@organisation)
 
@@ -31,17 +33,17 @@ class Organisation::MissionController < ApplicationController
   end
 
   private
-  def organisation_mission_params
+  def organisation_params
 
-    # When no checkbox is selected on the page no mission key/value is passed in the form,
-    # meaning that the organisation hash is no longer passed through either. In this case, we
-    # need to add it manually to avoid triggering a 'param is missing or value is empty'
-    # exception
-    if !params[:organisation].present?
+    # When no checkbox is selected on the page no mission key/value is passed
+    # in the form, meaning that the organisation hash is no longer passed
+    # through either. In this case, we need to add it manually to avoid
+    # triggering a 'param is missing or value is empty' exception
+    unless params[:organisation].present?
       params.merge!({organisation: {mission: ""}})
     end
 
-    params.require(:organisation).permit(mission: [])
+    params.require(:organisation).permit(:mission, mission: [])
 
   end
 
