1a2004864adb038fb092de431896f97c6df69fd8
Stuart McColl <contact@stuartmccoll.co.uk>
2020-02-27 09:26:02 +0000

    Added integration tests for project difference controller

---
+++ app/controllers/project/difference_controller.rb
@@ -0,0 +1,43 @@
+class Project::DifferenceController < ApplicationController
+  include ProjectContext, ObjectErrorsLogger
+
+  def update
+
+    logger.info "Updating difference for project ID: #{@project.id}"
+
+    @project.validate_difference = true
+
+    @project.update(project_params)
+
+    if @project.valid?
+
+      logger.info "Finished updating difference for project ID: #{@project.id}"
+
+      redirect_to :three_to_ten_k_project_matter_get
+
+    else
+
+      logger.info "Validation failed when attempting to update difference " \
+                  "for project ID: #{@project.id}"
+
+      log_errors(@project)
+
+      render :show
+
+    end
+
+  end
+
+  private
+
+  def project_params
+
+    unless params[:project].present?
+      params.merge!({project: {difference: ""}})
+    end
+
+    params.require(:project).permit(:difference)
+
+  end
+
+end
