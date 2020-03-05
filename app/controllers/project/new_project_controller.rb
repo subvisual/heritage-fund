3f778de53ae3b83335646f2334de2582c1530b74
Stuart McColl <contact@stuartmccoll.co.uk>
2020-03-05 10:10:58 +0000

    Added integration test for new project controller

--- app/controllers/project/new_project_controller.rb
+++ app/controllers/project/new_project_controller.rb
@@ -2,17 +2,17 @@ class Project::NewProjectController < ApplicationController
   before_action :authenticate_user!
 
   # This method creates a new project and then redirects the
-  # user to the first page of the 'About your project' journey
+  # to :three_to_ten_k_project_title_get
   def create_new_project
 
-    logger.info "User ID: #{current_user.id} has selected to " +
-                     "start a new project"
+    logger.info "User ID: #{current_user.id} has selected to start a " \
+                "new project"
 
     @project = Project.new(user: current_user)
     @project.save
 
-    logger.debug "Project created for user ID: #{current_user.id} " +
-                     "with ID: #{@project.id}"
+    logger.info "Project created for user ID: #{current_user.id} with ID: " \
+                "#{@project.id}"
 
     redirect_to three_to_ten_k_project_title_get_path(project_id: @project.id)
 
