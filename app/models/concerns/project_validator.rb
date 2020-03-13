45a3993d0d64712074da4ece4b4d11ead927bd1e
Stuart McColl <contact@stuartmccoll.co.uk>
2020-03-13 10:07:41 +0000

    Added integration tests for project dates controller

--- app/models/concerns/project_validator.rb
+++ app/models/concerns/project_validator.rb
@@ -3,26 +3,44 @@ class ProjectValidator < ActiveModel::Validator
   def validate(record)
 
     if !Date.valid_date? record.start_date_year.to_i, record.start_date_month.to_i, record.start_date_day.to_i
-      record.errors.add(:start_date, "Enter a valid project start date")
+      record.errors.add(
+          :start_date,
+          I18n.t("activerecord.errors.models.project.attributes.start_date.invalid")
+      )
     else
       unless Date.new(record.start_date_year.to_i, record.start_date_month.to_i, record.start_date_day.to_i) > Date.today
-        record.errors.add(:start_date, "Start date cannot be in past")
+        record.errors.add(
+            :start_date,
+            I18n.t("activerecord.errors.models.project.attributes.start_date.in_past")
+        )
       end
     end
 
     if !Date.valid_date? record.end_date_year.to_i, record.end_date_month.to_i, record.end_date_day.to_i
-      record.errors.add(:end_date, "Enter a valid project end date")
+      record.errors.add(
+          :end_date,
+          I18n.t("activerecord.errors.models.project.attributes.end_date.invalid")
+      )
     else
       unless Date.new(record.end_date_year.to_i, record.end_date_month.to_i, record.end_date_day.to_i) > Date.today
-        record.errors.add(:end_date, "End date cannot be in past")
+        record.errors.add(
+            :end_date,
+            I18n.t("activerecord.errors.models.project.attributes.end_date.in_past")
+        )
       end
     end
 
     unless record.errors.key?(:start_date) or record.errors.key?(:end_date)
       unless Date.new(record.end_date_year.to_i, record.end_date_month.to_i, record.end_date_day.to_i) >
           Date.new(record.start_date_year.to_i, record.start_date_month.to_i, record.start_date_day.to_i)
-        record.errors.add(:start_date, "Start date cannot be after end date")
-        record.errors.add(:end_date, "End date cannot be before start date")
+        record.errors.add(
+            :start_date,
+            I18n.t("activerecord.errors.models.project.attributes.start_date.after_end_date")
+        )
+        record.errors.add(
+            :end_date,
+            I18n.t("activerecord.errors.models.project.attributes.end_date.before_start_date")
+        )
       end
     end
 
