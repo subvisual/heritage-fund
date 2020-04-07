b344561ff917ca9562b18b080c1ac53007a235c5
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2020-04-07 11:17:33 +0100

    perform upsert if same form type sent multiple times

--- app/jobs/release_form_job.rb
+++ app/jobs/release_form_job.rb
@@ -6,10 +6,12 @@ class ReleaseFormJob < ApplicationJob
   def perform(params, raw_post)
     @project = Project.find(params[:ApplicationId])
     if params[:GrantDecision] == 'Awarded' && !params[:ApprovedbyFinance]
-      @project.released_forms.create(payload: raw_post, form_type: :permission_to_start)
+      @project.released_forms.find_or_initialize_by(project: @project, form_type: :permission_to_start)
+          .update!(payload: raw_post, form_type: :permission_to_start)
     end
     if params[:ApprovedbyFinance]
-      @project.released_forms.create(payload: raw_post, form_type: :completion_report)
+      @project.released_forms.find_or_initialize_by(project: @project, form_type: :completion_report)
+          .update!(payload: raw_post, form_type: :completion_report)
     end
   end
 end
