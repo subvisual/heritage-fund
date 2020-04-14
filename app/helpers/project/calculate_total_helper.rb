0f6b5749bc6e8fa66f699a9c5f7083b3433d6f12
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2020-04-14 13:48:51 +0100

    use sum rails helper instead of reduce

--- app/helpers/project/calculate_total_helper.rb
+++ app/helpers/project/calculate_total_helper.rb
@@ -1,5 +1,5 @@
 module Project::CalculateTotalHelper
   def calculate_total(object)
-    object.select(:amount).map(&:amount).compact.reduce(:+)
+    object.select(:amount).map(&:amount).compact.sum
   end
 end
