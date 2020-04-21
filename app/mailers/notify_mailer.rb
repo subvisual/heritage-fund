a7a2e8cc3ef5631c63391fc31f2b31e0e97e2c49
Stuart McColl <contact@stuartmccoll.co.uk>
2020-04-21 15:29:20 +0100

    Revert "Reinstates Devise asynchronous sending (#386)"
    This reverts commit 12a788058aef7b1da475f056a1ca87eace892d2e.

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -4,9 +4,6 @@ class NotifyMailer < Mail::Notify::Mailer
 
   include Devise::Controllers::UrlHelpers
 
-  def default_url_options
-    Rails.configuration.action_mailer.default_url_options
-  end
 
   def confirmation_instructions(record, token, opts = {})
     template_mail('a44293b7-7263-42b4-8905-44bbedaf1dfa',
