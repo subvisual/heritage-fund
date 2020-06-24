917a462104575331ebeda0ede133b12ddfa40e8b
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2020-06-24 11:56:41 +0100

    Updated address controller to associate person with user address
    This commit updates the address controller to create a new
    Address object when updating a user's address and to then
    associate the user's associated person with this address.

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -9,9 +9,10 @@ class AddressController < ApplicationController
 
   end
 
-  # This method updates the mission attribute of an organisation,
-  # redirecting to :organisation_mission if successful and re-rendering
-  # :about method if unsuccessful
+  # This method updates the address for a given model object, which could 
+  # be of types user, organisation or project. If of type user, then the 
+  # address attributes are replicated to a separate address record which is
+  # then associated with the user and it's associated person record
   def update
 
     logger.info "Updating address for #{@type} ID: #{@model_object.id}"
@@ -30,7 +31,11 @@ class AddressController < ApplicationController
       elsif @type == "project"
         redirect_to three_to_ten_k_project_description_path(params['id'])
       elsif @type == "user"
+
+        check_and_set_person_address(@model_object)
+
         redirect_to :authenticated_root
+
       end
 
     else
@@ -68,6 +73,71 @@ class AddressController < ApplicationController
     else
       redirect_to :root
     end
+  end
+
+  # Checks for the presence of an associated Address via the PeopleAddresses
+  # model. If none exists, a new Address and PeopleAddress is created. Address
+  # details are replicated from the current_user object
+  #
+  # @param [User] user An instance of User
+  def check_and_set_person_address(user)
+
+    person_address_association = PeopleAddress.find_by(person_id: user.person_id)
+
+    unless person_address_association
+
+      logger.debug "No people_addresses record found for person ID: #{user.person_id}"
+
+      address = Address.create
+
+      logger.debug "addresses record created with ID: #{address.id}"
+
+      person_address_association = create_person_address_association(user.person_id, address.id)
+
+    end
+
+    replicate_address_from_current_user_details(person_address_association.address_id, user)
+
+  end
+
+  # Creates and returns a new Address object based on the 
+  # attributes stored against the User argument
+  #
+  # @param [User] user An instance of User
+  def replicate_address_from_current_user_details(id, user)
+
+    address = Address.find(id)
+
+    address.update(
+      line1: user.line1,
+      line2: user.line2,
+      line3: user.line3,
+      town_city: user.townCity,
+      county: user.county,
+      postcode: user.postcode
+    )
+
+  end
+
+  # Creates and returns a PeopleAddress object based on the 
+  # Address and User arguments passed
+  #
+  # @param [uuid] person_id The unique identifier of a Person
+  # @param [uuid] address_id The unique identifier of an Address
+  def create_person_address_association(person_id, address_id)
+
+    logger.debug "Creating people_addresses record for person ID: #{person_id} " \
+      "and address ID: #{address_id}"
+
+    person_address_association = PeopleAddress.create(
+      person_id: person_id,
+      address_id: address_id
+    )
+
+    logger.debug "people_addresses record created with ID: #{person_address_association.id}"
+
+    return person_address_association
+
   end
 
   def model_params
