b861b029d4005d3fed3a226027874ee670ca668b
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2020-08-13 07:47:52 +0100

    Linted new_application_controller.rb
    This commit contains linting fixes for
    new_application_controller.rb.

--- app/controllers/new_application_controller.rb
+++ app/controllers/new_application_controller.rb
@@ -1,5 +1,7 @@
+# Controller responsible for orchestrating the journey for a user
+# who has selected to create a new application
 class NewApplicationController < ApplicationController
-    include ObjectErrorsLogger
+  include ObjectErrorsLogger
 
   # If the current_user has no linked organisation, or if their linked organisation
   # does not contain all mandatory details, then redirect to the dashboard
@@ -12,9 +14,9 @@ class NewApplicationController < ApplicationController
 
   end
 
-  # This method creates a new NewApplication object and then uses this to 
-  # validate the application_type selected on the user. If validation passes, 
-  # then the user is redirected to the application start page relevant to 
+  # This method creates a new NewApplication object and then uses this to
+  # validate the application_type selected on the user. If validation passes,
+  # then the user is redirected to the application start page relevant to
   # the application_type selected. Otherwise, :show is re-rendered
   def update
 
@@ -34,7 +36,7 @@ class NewApplicationController < ApplicationController
 
     else
 
-      logger.info "Validation failed when attempting to select application_type " \
+      logger.info 'Validation failed when attempting to select application_type ' \
                     "for user ID: #{current_user.id}"
 
       log_errors(@application)
@@ -49,31 +51,33 @@ class NewApplicationController < ApplicationController
 
   def new_application_params
 
-    # When no radio button is selected on the page no application_type key/value is 
-    # passed in the form, meaning that the new_application hash is no longer passed 
-    # through either. In this case, we need to add it manually to avoid triggering 
-    # a 'param is missing or value is empty' exception
-    params.merge!(new_application: {application_type: ""}) unless
+    # When no radio button is selected on the page no application_type
+    # key/value is passed in the form, meaning that the new_application hash
+    # is no longer passed through either. In this case, we need to add it
+    # manually to avoid triggering a 'param is missing or value is empty'
+    # exception
+    params.merge!(new_application: { application_type: '' }) unless
         params[:new_application].present?
 
     params.require(:new_application).permit(:application_type)
 
   end
 
-  # Redirects the user based on the presence of and completeness of their associated 
-  # organisation. If not present or incomplete, we redirect to the dashboard
-  # orchestration route
+  # Redirects the user based on the presence of and completeness of their
+  # associated organisation. If not present or incomplete, we redirect to
+  # the dashboard orchestration route
   #
   # @param [User] user An instance of User
   def redirect_based_on_organisation_presence_and_completeness(user)
 
-    unless user.organisation && helpers.complete_organisation_details?(user.organisation)
+    unless user.organisation &&
+           helpers.complete_organisation_details?(user.organisation)
       redirect_to :orchestrate_dashboard_journey
     end
 
   end
 
-  # This method will redirect the user to the correct application start page 
+  # This method will redirect the user to the correct application start page
   # based on the application_type that they have selected
   #
   # @param [NewApplication] application An instance of NewApplication
@@ -83,27 +87,27 @@ class NewApplicationController < ApplicationController
 
     when 'hef_loan'
 
-        logger.info "Redirecting to HEF Loan form for user ID: #{current_user.id}"
+      logger.info "Redirecting to HEF Loan form for user ID: #{current_user.id}"
 
-        redirect_to :funding_application_hef_loan_start
+      redirect_to :funding_application_hef_loan_start
 
     when 'sff_small'
 
-        logger.info "Redirecting to SFF Small form for user ID: #{current_user.id}"
+      logger.info "Redirecting to SFF Small form for user ID: #{current_user.id}"
 
-        redirect_to :three_to_ten_k_project_start
+      redirect_to :three_to_ten_k_project_start
 
     else
-        logger.info 'Method redirect_to_application_start_page called with ' \
-                    "an application object containing an " \
-                    "application_type of #{application.application_type}"
+      logger.info 'Method redirect_to_application_start_page called with ' \
+                  'an application object containing an ' \
+                  "application_type of #{application.application_type}"
 
-        # If the user has managed to hit this route with an invalid 
-        # application_type, then we should redirect them back to root
-        redirect_to :authenticated_root
+      # If the user has managed to hit this route with an invalid
+      # application_type, then we should redirect them back to root
+      redirect_to :authenticated_root
 
     end
 
   end
-    
+
 end
