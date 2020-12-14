7229167f01e0bb9a4440826d54e013bcde30158a
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2020-12-14 11:44:01 +0000

    Linted title_controller.rb
    This commit contains fixes to RuboCop linting errors to the
    title_controller.rb file.

--- app/controllers/funding_application/gp_project/title_controller.rb
+++ app/controllers/funding_application/gp_project/title_controller.rb
@@ -1,5 +1,7 @@
+# Controller for the project title page of the funding application journey
 class FundingApplication::GpProject::TitleController < ApplicationController
-  include FundingApplicationContext, ObjectErrorsLogger
+  include FundingApplicationContext
+  include ObjectErrorsLogger
 
   # This method updates the project_title attribute of a project,
   # redirecting to :three_to_ten_k_project_key_dates if successful and
@@ -14,14 +16,14 @@ class FundingApplication::GpProject::TitleController < ApplicationController
 
     if @funding_application.project.valid?
 
-      logger.info "Finished updating project_title for project ID: " \
+      logger.info 'Finished updating project_title for project ID: ' \
                   "#{@funding_application.project.id}"
 
       redirect_to :funding_application_gp_project_key_dates
 
     else
 
-      logger.info "Validation failed when attempting to update project_title " \
+      logger.info 'Validation failed when attempting to update project_title ' \
                   " for project ID: #{@funding_application.project.id}"
 
       log_errors(@funding_application.project)
