09a8c5706a2c3131038202694eb33541de8a4f02
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2021-01-07 09:44:27 +0000

    Updated PreApplicationContext to fix additional access case
    This commit updates the PreApplicationContext to consider additional access cases where a user could try to access a leg of the journey using a PreApplicationContext which does not have the correct associated object.

--- app/controllers/concerns/pre_application_context.rb
+++ app/controllers/concerns/pre_application_context.rb
@@ -1,35 +1,73 @@
 # Controller concern used to set the @pre_application
 # instance variable
 module PreApplicationContext
-    extend ActiveSupport::Concern
-    included do
-      before_action :authenticate_user!, :set_pre_application
-    end
-  
-    # This method retrieves a PreApplication object based on the id
-    # found in the URL parameters and the current authenticated
-    # user's id, setting it as an instance variable for use in
-    # pre application-related controllers.
-    #
-    # If no PreApplication object matching the parameters is found,
-    # then the user is redirected to the applications dashboard.
-    def set_pre_application
-  
-      @pre_application = PreApplication.find_by(
-        id: params[:pre_application_id],
-        organisation_id: current_user.organisations.first&.id
-      )
-  
-      if !@pre_application.present? || (@pre_application.submitted_on.present? &&
-          !request.path.include?('/submitted'))
-  
-          logger.error("User redirected to root, error in pre_application_context.rb")
-  
-        redirect_to :authenticated_root
-  
-      end
-  
-    end
-  
+  extend ActiveSupport::Concern
+  included do
+    before_action :authenticate_user!, :set_pre_application
   end
-  
+
+  # This method retrieves a PreApplication object based on the id
+  # found in the URL parameters and the current authenticated
+  # user's id, setting it as an instance variable for use in
+  # pre application-related controllers.
+  #
+  # If no PreApplication object matching the parameters is found,
+  # then the user is redirected to the applications dashboard.
+  def set_pre_application
+
+    @pre_application = PreApplication.find_by(
+      id: params[:pre_application_id],
+      organisation_id: current_user.organisations.first&.id
+    )
+
+    if !@pre_application.present? || (@pre_application.submitted_on.present? &&
+        !request.path.include?('/submitted'))
+
+      logger.error('User redirected to root, error in pre_application_context.rb')
+
+      redirect_to :authenticated_root
+
+    else
+
+      # A PreApplication GUID is used to access both project enquiry and expression
+      # of interest pre-application journeys. We should ensure that a user is not
+      # able to access one type of journey using a PreApplication GUID which has
+      # an associated object related to the other type of journey (e.g. using a
+      # PreApplication GUID with an associated PaExpressionOfInterest object to
+      # access the project enquiry journey)
+
+      if request.path.include?('expression-of-interest')
+
+        unless @pre_application.pa_expression_of_interest.present?
+
+          logger.error(
+            'User redirected to root, attempted to access /expression-of-interest ' \
+            'URI using PreApplication object with no associated PaExpressionOfInterest'
+          )
+
+          redirect_to :authenticated_root
+
+        end
+
+      end
+
+      if request.path.include?('project-enquiry')
+
+        unless @pre_application.pa_project_enquiry.present?
+
+          logger.error(
+            'User redirected to root, attempted to access /project-enquiry URI ' \
+            'using PreApplication object with no associated PaProjectEnquiry'
+          )
+
+          redirect_to :authenticated_root
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
