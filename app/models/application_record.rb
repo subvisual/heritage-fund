368799b5e365af9f60870a9f81484962ea8df17e
Miguel Palhas <mpalhas@gmail.com>
2021-01-11 16:07:55 +0000

    code review

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,16 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   nilify_blanks
-
-  def update(params)
-    super params
-  rescue ActiveRecord::MultiparameterAssignmentErrors => e
-    e.errors.each do |error|
-      errors.add(
-        error.attribute,
-        I18n.t("activerecord.errors.models.#{self.class.name.underscore}.attributes.#{error.attribute}.invalid")
-      )
-    end
-    false
-  end
 end
